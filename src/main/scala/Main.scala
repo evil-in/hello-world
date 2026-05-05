28bca74d06d128199e8b728a74ac395a7c926d62
Preethi-Evelyn-Sadanandan <preethievelynsadanandan@gmail.com>
2026-05-05 14:31:22 +0530

    Completed tutorial on lists

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import Example.test_sol
 import UnitedKingdom.*
 
 @main def hello(): Unit =
@@ -22,12 +23,24 @@ import UnitedKingdom.*
   println(whereAmI(Scotland))
   println(middleName.getOrElse("").length) // to get the value out of a option and get its length
   println(upper_middle_name)
-  println(mulan.name)
-  println(mulan.rating)
-  println(mulan.rating.map(rating => rating.googleRating))
-  println(mulan.rating.map(rating => rating.googleRating).flatten)
-  println(mulan.rating.flatMap(rating => rating.googleRating))
-
+  println(Example.mulan.name)
+  println(Example.mulan.rating)
+  println(Example.mulan.rating.map(rating => rating.googleRating))
+  println(Example.mulan.rating.map(rating => rating.googleRating).flatten)
+  println(Example.mulan.rating.flatMap(rating => rating.googleRating))
+  println(test_sol.map(n => n*2))
+  println(numbers.map(n => n * 2))
+  println(names.map(name => name.toUpperCase))
+  println(numbers.map(ns => List(ns, ns*2)).flatten)
+  println(numbers.flatMap(ns => List(ns, ns*2)))
+  println(list_eg.head)
+  println(list_eg.tail)
+  println(list_eg.distinct)
+  println(list_eg.take(3))
+  println(list_eg.length)
+  println(list_eg.map(n => n * n))
+  println(list_eg.flatMap(n => List.fill(3)(n)))
+  println(nearby_trips(destination_list))
 
 def msg = "I was compiled by Scala 3. :)"
 
@@ -94,10 +107,53 @@ val middleName: Option[String] = Some("Jasmine")
 val upper_middle_name = middleName.map(word => word.toUpperCase)
 
 // flatmap
-case class Rating(googleRating: Option[Int])
-case class Film(name: String, rating: Option[Rating])
-val mulan = Film("Mulan", Some(Rating(Some(5))))
+object Example {
+  case class Rating(googleRating: Option[Int], netflixRating: Option[Int])
 
-// for comprehensions
+  case class Film(name: String, rating: Option[Rating])
+
+  val mulan = Film("Mulan", Some(Rating(Some(5), Some(4))))
+
+  // for comprehensions
+  val avg_rating = for {
+    rating <- mulan.rating
+    googleRating <- rating.googleRating
+    netflixRating <- rating.netflixRating
+    avg_rating = (googleRating + netflixRating)/2
+  } yield avg_rating
+
+  val test_sol: Option[Int] = Some(5)
+
+//  test_sol.map(n => n*2)
+
+  // challenge: case class
+  case class TicketPrice(standardTicketPrice: Int, firstClassTicketPrice: Option[Int])
+
+  def getFirstClassTicketPrice(ticketPrice: Option[TicketPrice]): Option[Int] =
+    ticketPrice.flatMap(_.firstClassTicketPrice)
+}
+
+val numbers = List(1,2,3,4,5)
+val names = List("Moana", "Jasmine")
+
+val list_eg: List[Int] = List(1,1,2,2,3,3,4)
+
+case class Destination(city: String, journeyTime: Int)
+
+val destination_list = List(
+    Destination("Malaysia", 30),
+    Destination("Singapore", 50),
+    Destination("Canada", 80),
+    Destination("UK", 70),
+    Destination("Ireland", 40),
+    Destination("Australia", 35)
+  )
+
+def nearby_trips(destinations: List[Destination]): List[String] =
+  for
+    destination <- destinations
+    if destination.journeyTime < 60
+  yield destination.city
+end nearby_trips
 
 
